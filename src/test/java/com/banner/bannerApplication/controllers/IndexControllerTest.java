a76c89a4b7f9831f2b2c97919a5c439f72c4e741
Jason <jason.gambino3@gmail.com>
2017-09-30 16:33:47 -0600

    Another one

--- src/test/java/com/banner/bannerApplication/controllers/IndexControllerTest.java
+++ src/test/java/com/banner/bannerApplication/controllers/IndexControllerTest.java
@@ -1,6 +1,6 @@
 package com.banner.bannerApplication.controllers;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
--- src/test/java/com/banner/bannerApplication/entities/CourseTest.java
+++ src/test/java/com/banner/bannerApplication/entities/CourseTest.java
@@ -1,6 +1,6 @@
 package com.banner.bannerApplication.entities;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
