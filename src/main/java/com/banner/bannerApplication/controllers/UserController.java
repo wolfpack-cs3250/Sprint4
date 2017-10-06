fd14c1f0e9327e4c9681c0d5fd3173c568cb9e33
sal <sal2993@hotmail.com>
2017-10-06 01:08:09 -0600

    Got all the CRUD Operations to work Properly

--- src/main/java/com/banner/bannerApplication/controllers/UserController.java
+++ src/main/java/com/banner/bannerApplication/controllers/UserController.java
@@ -59,16 +59,17 @@ public class UserController {
 
 
 
-    // UPDATE
+    // UPDATE page
     @GetMapping(path="/update/{id}")
     public String updateUser(@PathVariable Long id,
                                            Model model) {
+        // Needs Error Checking!!
         User user = userRepository.findOne(id);
         model.addAttribute("student", user);
         return "update";
     }
 
-    // UPDATE
+    // UPDATE User
     @GetMapping(path="/update")
     public ModelAndView updateStudent(@RequestParam Long id,
                                    @RequestParam String firstname,
