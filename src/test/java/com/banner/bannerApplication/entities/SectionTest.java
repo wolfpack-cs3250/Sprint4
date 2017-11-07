ca6199b3b2482749550fac476a76227d56d17580
JasonTheMain <jason.gambino3@gmail.com>
2017-11-07 09:17:19 -0700

    Update SectionTest.java

--- src/test/java/com/banner/bannerApplication/entities/SectionTest.java
+++ src/test/java/com/banner/bannerApplication/entities/SectionTest.java
@@ -17,18 +17,7 @@ public class SectionTest {
 
        section.setSectionNumber(1);
     }
-        
-//     @Test
-//     public void getCourseId()throws Exception {
-//     assertEquals(1, course.getCourseId());
-//     }
-
-//     @Test
-//     public void setCourseId()throws Exception {
-//         course.setCourseId(2);
-//         assertEquals(2,course.getCourseId());
-//     }
-
+       
     @Test
     public void getCourseName()throws Exception {
         assertEquals("A",course.getCourseName());
@@ -38,19 +27,6 @@ public class SectionTest {
     public void setCourseName() throws Exception{
         course.setCourseName("Yeah");
         assertEquals("Yeah",course.getCourseName());
-    }
-
-    @Test
-    public void getSectionNumber() throws Exception {
-        Long aLong = 1;
-        assertEquals(1, section.getSectionNumber(aLong));
-    }
-
-    @Test
-    public void setSectionNumber() throws Exception {
-        Long aLong = 1;
-        section.setSectionNumber(3);
-        assertEquals(3, section.getSectionNumber(aLong));
     }
 
 }
