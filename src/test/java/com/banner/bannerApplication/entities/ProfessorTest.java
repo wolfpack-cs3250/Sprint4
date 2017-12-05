745301c54393fe4c7f485b30a2d213f3a545bffd
hexphase <13786214+hexphase@users.noreply.github.com>
2017-12-04 17:02:27 -0700

    Fixed compile time error in ProfessorTest.java, fixed UserTest.java, added test comments throughout, cleaned up formatting.

--- src/test/java/com/banner/bannerApplication/entities/ProfessorTest.java
+++ src/test/java/com/banner/bannerApplication/entities/ProfessorTest.java
@@ -1,12 +1,16 @@
 package com.banner.bannerApplication.entities;
 
-import com.sun.scenario.effect.impl.prism.PrFilterContext;
 import org.junit.Before;
 import org.junit.Test;
 
+// This is the test class for Professor objects
+
 import static org.junit.Assert.*;
 public class ProfessorTest {
+
     private Professor professor;
+
+    // perform our initial set up of our Professor object
     @Before
     public void setUP(){
         professor=new Professor();
@@ -14,22 +18,26 @@ public class ProfessorTest {
         professor.setLastName("Skywalker");
     }
 
+    // try to get the professors first name
     @Test
     public void getFirstName() throws Exception{
         assertEquals("anakin",professor.getFirstName());
     }
 
+    // try to set the professors first name
     @Test
     public void setFirstName() throws Exception{
         professor.setFirstName("jake");
         assertEquals("jake",professor.getFirstName());
     }
 
+    // try to get the professors last name
     @Test
     public void getLastName() throws Exception{
         assertEquals("Skywalker",professor.getLastName());
     }
 
+    // try to set the professors last name
     @Test
     public void setLastName() throws Exception{
         professor.setLastName("anakinBleack");
--- src/test/java/com/banner/bannerApplication/entities/SectionTest.java
+++ src/test/java/com/banner/bannerApplication/entities/SectionTest.java
@@ -4,28 +4,34 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+// This is the test class for Section objects
+
 public class SectionTest {
+
+    // perform our initial set up of our Course and Section objects
     private Course course;
     private Section section;
 
+    // perform our initial set up of our Section object
     @Before
     public void setUp(){
         course = new Course();
         section = new Section();
-        course.setCourseName("A");
-
-       section.setSectionNumber(1);
+        course.setCourseName("CS3250");
+        section.setSectionNumber(1);
     }
-       
+
+    // Make sure we can access the Section objects course name
     @Test
     public void getCourseName()throws Exception {
-        assertEquals("A",course.getCourseName());
+        assertEquals("CS3250",course.getCourseName());
     }
 
+    // Make sure we can set/change the courses name
     @Test
     public void setCourseName() throws Exception{
-        course.setCourseName("Yeah");
-        assertEquals("Yeah",course.getCourseName());
+        course.setCourseName("CS1050");
+        assertEquals("CS1050",course.getCourseName());
     }
 
 }
