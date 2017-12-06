e02c2e7716ac7cb7a66039d45ed5531c9179779d
gdinhvu81 <31318110+gdinhvu81@users.noreply.github.com>
2017-12-06 15:49:18 -0700

    Add files via upload

---
+++ src/test/java/com/banner/bannerApplication/entities/RoomsTest.java
@@ -0,0 +1,43 @@
+package com.banner.bannerApplication.entities;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RoomsTest {
+
+    private Rooms rooms;
+
+    @Before
+    public void setUp(){
+        rooms = new Rooms();
+        rooms.setRoomSize("123");
+        rooms.setRoomNumber("123");
+    }
+
+
+    @Test
+    public void getRoomSize() throws Exception {
+        Assert.assertEquals("123", rooms.getRoomSize());
+    }
+
+    @Test
+    public void setRoomSize() throws Exception {
+        rooms.setRoomSize("123");
+        assertEquals("123", rooms.getRoomSize());
+    }
+
+    @Test
+    public void getRoomNumber() throws Exception {
+        assertEquals("123", rooms.getRoomNumber());
+    }
+
+    @Test
+    public void setRoomNumber() throws Exception {
+        rooms.setRoomNumber("123");
+        assertEquals("123", rooms.getRoomNumber());
+    }
+
+}
